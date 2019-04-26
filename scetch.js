75cf414cc0e92e843b29f38bb06f3c4e04b9a848
Yanick <yanick53@live.nl>
2019-04-26 18:05:12 +0200

    added buttons for stopping and resetting the visualisation

--- scetch.js
+++ scetch.js
@@ -1,10 +1,19 @@
 var w = 800;
 var h = 500;
 var data;
+var stopbutton;
+var startbutton;
+var start_sort = false;
 
 function setup() {
   createCanvas(w, h);
   data = createArr();
+  stopbutton = createButton("Reset visualisation");
+  stopbutton.position(0, h + 20);
+  stopbutton.mousePressed(stopSort);
+  startbutton = createButton("Start Bubblesort");
+  startbutton.position(0, h + 50);
+  startbutton.mousePressed(startSort);
 }
 
 function draw() {
@@ -14,7 +23,18 @@ function draw() {
   for (var i = 0; i < data.length; i++) {
     line(i, 0, i, -data[i]);
   }
-  bubbleSort();
+  if (start_sort) {
+    bubbleSort();
+  }
+}
+
+function startSort() {
+  start_sort = true;
+}
+
+function stopSort() {
+  start_sort = false;
+  data = createArr();
 }
 
 function bubbleSort() {
